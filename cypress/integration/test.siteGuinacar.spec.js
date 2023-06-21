e2c63d4f57ae3aaa3f1c693c1f4bdfa02e1506d7
leocostatmt <leonardocaetanocosta2005@gmail.com>
2023-06-21 12:09:10 -0300

    modified:   cypress/integration/test.siteGuinacar.spec.js

--- cypress/integration/test.siteGuinacar.spec.js
+++ cypress/integration/test.siteGuinacar.spec.js
@@ -19,7 +19,13 @@ describe('Central de Atendimento ao Cliente TAT' , function() {
         cy.visit('www.guinacarautomotive.com.br')
     })
 
-    it.only('verifica se o site está online' , function(){
+    //DEVE DAR CERTO
+    it('verifica se o site está online 1' , function(){
+        cy.title().should('be.equal' , 'Guinacar Automotive | Loja Online')
+    })
+
+    //DEVE DAR ERRADO
+    it('verifica se o site está online 2' , function(){
         cy.title().should('not.be.equal' , 'Guinacar Automotive | Loja Online')
     })
 
