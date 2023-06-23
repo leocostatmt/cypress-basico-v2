9b0e131a3c8982badec60ef28474e94d72730cd9
Léo Costa <108466384+leocostatmt@users.noreply.github.com>
2023-06-22 21:37:50 -0300

    modified:   cypress/integration/test.siteGuinacar.spec.js

--- cypress/integration/test.siteGuinacar.spec.js
+++ cypress/integration/test.siteGuinacar.spec.js
@@ -1,17 +1,5 @@
 
 
-
-// CAC.CAT.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
-
-/// <reference types="Cypress" />
-
-
-
 describe('Central de Atendimento ao Cliente TAT' , function() {
     //const THREE_SECONDS_IN_MS = 3000
     
