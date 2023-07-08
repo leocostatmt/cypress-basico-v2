025e5a27ae9c5c486bd8c06e177a0214a35f9246
leocostatmt <leonardocaetanocosta2005@gmail.com>
2023-07-08 14:59:43 -0300

    modified:   cypress/integration/test.siteGuinacar.spec.js

--- cypress/integration/test.siteGuinacar.spec.js
+++ cypress/integration/test.siteGuinacar.spec.js
@@ -1,12 +1,4 @@
 
-
-// CAC.CAT.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
-
 /// <reference types="Cypress" />
 
 // NESTES TESTES SÃO UTILIZADOS TDD (TEST-DRIVEN-DEVELOPMENT)
